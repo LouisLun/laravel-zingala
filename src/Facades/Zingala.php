23ab00fa4791952a9520b1ab3d7346d954df5f77
LouisLun <louis.jhan@area02.com>
2024-08-23 09:01:55 +0800

    modify: update document

--- src/Facades/Zingala.php
+++ src/Facades/Zingala.php
@@ -3,6 +3,14 @@ namespace LouisLun\LaravelZingala\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \LouisLun\LaravelZingala\Response payment($params) 向Zingala請求付款
+ * @method static \LouisLun\LaravelZingala\Response refund($params) 退款
+ * @method static \LouisLun\LaravelZingala\Response inquiry($params) 查詢交易紀錄
+ * @method static \LouisLun\LaravelZingala\Crypto crypto() 加解密器
+ *
+ * {@see \LouisLun\LaravelZingala\Zingala}
+ */
 class Zingala extends Facade
 {
     /**
